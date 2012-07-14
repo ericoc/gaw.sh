3b8cab3c9757fd1371366e47e639de3a0b5cbc46
Eric OC <eric1207@gmail.com>
2012-07-14 15:08:23 -0400

    have blcheck use new big checkURL function,  turn on error reporting, show more information, and fix bad urls count

--- admin/blcheck.php
+++ admin/blcheck.php
@@ -2,6 +2,7 @@
 
 // Turn on debugging if asked to
 if ( (isset($_GET['debug'])) && ($_GET['debug'] == 'yes') ) {
+	error_reporting('E_ALL');
 	$starttime = microtime(true);
         $debug = TRUE;
 	echo <<< END
@@ -28,7 +29,7 @@ $link = mysql_connect($sqlhost, $sqluser, $sqlpass) OR die('Cannot connect to DB
 mysql_select_db($sqldb, $link);
 
 // Query for enabled URLs
-$geturls = mysql_query("SELECT id, url FROM `urls` WHERE `status` = '1'", $link);
+$geturls = mysql_query("SELECT id, alias, url FROM `urls` WHERE `status` = '1'", $link);
 
 // Start out and begin counting bad URLs
 if ($debug) {
@@ -39,55 +40,36 @@ $badurls = 0;
 // Loop through every enabled URL
 while ($row = mysql_fetch_array($geturls)) {
 
-	// Get the URL id and actual long URL
+	// Get the URL id , alias, and actual (long) URL
 	$id = $row['id'];
+	$alias = $row['alias'];
 	$url = $row['url'];
 
+	// Tell us what's being checked if debug is on
 	if ($debug) {
-		echo "Checking URL \"$url\" (ID $id)... ";
+		echo "Checking URL \"$url\" (ID $id / $alias)... ";
 	}
 
-	// Determine domain name
-	$domain = parse_url($url, PHP_URL_HOST);
+	// Actually check the URL
+	$error = checkURL($url);
 
-	// Check dumb domain list
-	if (isDumb(strtolower($domain))) {
-		$error = 'dumb domain';
+	// If the URL is bad, do something (increment the amount of fails and eventually disable entirely?)
+	if ( (isset($error)) && (!empty($error)) ) {
+		$badurls++;
 
-	// Check if URL is legit/works
-	} elseif (!isLegit($url)) {
-		$error = 'not legit';
+		// Tell us what the error was if debug is on
+		if ($debug) {
+			echo "<span id=\"error\">$error</span><br>\n";
+		}
 
-	// Check URL against Spamhaus' DBL
-	} elseif (isDBL($domain)) {
-		$error = 'dbl';
+		// Empty the variable so we can start the loop over
+		$error = "";
 
-	// Check URL against SURBL
-	} elseif (isSURBL($domain)) {
-		$error = 'surbl';
-
-	// Check URL against URIBL
-	} elseif (isURIBL($domain)) {
-		$error = 'uribl';
-
-	// Check URL and its nameservers against Spamhaus' ZEN
-	} elseif (isZEN($domain)) {
-		$error = 'spamhaus zen';
-
+	// Tell us the URL is okay if so and debug is on
 	} else {
 		if ($debug) {
 			echo "OK<br>\n";
 		}
-	}
-
-	// If the URL is bad, do something (increment the amount of fails and eventually disable entirely?)
-	if ( (isset($error)) && (!empty($error)) ) {
-		$badurl++;
-
-		if ($debug) {
-			echo "<span id=\"error\">$error</span><br>\n";
-		}
-		$error = "";
 	}
 }
 
@@ -98,7 +80,7 @@ mysql_close($link);
 if ($debug) {
 	$endtime = microtime(true);
 	$howlong = $endtime - $starttime;
-	echo "<br>Found $badurl bad URLs<br>\n";
+	echo "<br>Found $badurls bad URLs<br>\n";
 	echo "Took $howlong seconds<br>\n";
 	echo "Done.\n";
 	echo <<< END
