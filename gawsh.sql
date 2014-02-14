0aff09aed655dc77d3a7cb7c6a98c048e48f40b6
Eric OC <eric@ericoc.com>
2014-02-13 21:20:41 -0500

    SQL cleanup

--- gawsh.sql
+++ gawsh.sql
@@ -1,41 +1,7 @@
---
 -- Database: `gawsh`
---
-
 -- --------------------------------------------------------
 
---
--- Stand-in structure for view `recentvisits`
---
-DROP VIEW IF EXISTS `recentvisits`;
-CREATE TABLE IF NOT EXISTS `recentvisits` (
-`alias` varchar(50)
-,`FROM_UNIXTIME(visits.time)` datetime
-,`ip` varchar(128)
-,`referrer` text
-);
--- --------------------------------------------------------
-
---
--- Stand-in structure for view `searchurlsvisits`
---
-DROP VIEW IF EXISTS `searchurlsvisits`;
-CREATE TABLE IF NOT EXISTS `searchurlsvisits` (
-`id` int(10) unsigned
-,`alias` varchar(50)
-,`url` text
-,`ip` varchar(128)
-,`time` varchar(15)
-,`status` varchar(2)
-,`visits` bigint(21)
-);
--- --------------------------------------------------------
-
---
 -- Table structure for table `urls`
---
-
-DROP TABLE IF EXISTS `urls`;
 CREATE TABLE IF NOT EXISTS `urls` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `alias` varchar(50) NOT NULL,
@@ -48,7 +14,6 @@ CREATE TABLE IF NOT EXISTS `urls` (
   KEY `alias` (`alias`),
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-
 INSERT INTO `urls` (`id`, `alias`, `url`, `ip`, `time`, `status`) VALUES
 (1, 'admin', '', '127.0.0.1', '1323141765', '-1'),
 (2, '401', '', '127.0.0.1', '1323141766', '-1'),
@@ -57,42 +22,8 @@ INSERT INTO `urls` (`id`, `alias`, `url`, `ip`, `time`, `status`) VALUES
 (5, '410', '', '127.0.0.1', '1323141769', '-1'),
 (6, '503', '', '127.0.0.1', '1323141770', '-1');
 
--- --------------------------------------------------------
 
---
--- Stand-in structure for view `urlsbyid`
---
-DROP VIEW IF EXISTS `urlsbyid`;
-CREATE TABLE IF NOT EXISTS `urlsbyid` (
-`id` int(10) unsigned
-,`alias` varchar(50)
-,`url` text
-,`ip` varchar(128)
-,`time` varchar(15)
-,`status` varchar(2)
-,`visitors` bigint(21)
-);
--- --------------------------------------------------------
-
---
--- Stand-in structure for view `urlsbyvisits`
---
-DROP VIEW IF EXISTS `urlsbyvisits`;
-CREATE TABLE IF NOT EXISTS `urlsbyvisits` (
-`id` int(10) unsigned
-,`alias` varchar(50)
-,`url` text
-,`ip` varchar(128)
-,`time` varchar(15)
-,`status` varchar(2)
-,`visitors` bigint(21)
-);
--- --------------------------------------------------------
-
---
 -- Table structure for table `visits`
---
-
 DROP TABLE IF EXISTS `visits`;
 CREATE TABLE IF NOT EXISTS `visits` (
   `id` int(10) unsigned NOT NULL,
@@ -105,36 +36,14 @@ CREATE TABLE IF NOT EXISTS `visits` (
 
 -- --------------------------------------------------------
 
---
 -- Structure for view `recentvisits`
---
-DROP TABLE IF EXISTS `recentvisits`;
-
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `recentvisits` AS select `urls`.`alias` AS `alias`,from_unixtime(`visits`.`time`) AS `FROM_UNIXTIME(visits.time)`,`visits`.`ip` AS `ip`,`visits`.`referrer` AS `referrer` from (`visits` join `urls` on((`urls`.`id` = `visits`.`id`))) order by `visits`.`time` desc limit 100;
 
--- --------------------------------------------------------
-
---
 -- Structure for view `searchurlsvisits`
---
-DROP TABLE IF EXISTS `searchurlsvisits`;
-
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `searchurlsvisits` AS select `urls`.`id` AS `id`,`urls`.`alias` AS `alias`,`urls`.`url` AS `url`,`urls`.`ip` AS `ip`,`urls`.`time` AS `time`,`urls`.`status` AS `status`,count(`visits`.`id`) AS `visits` from (`urls` join `visits` on((`visits`.`id` = `urls`.`id`))) group by `visits`.`id`;
 
--- --------------------------------------------------------
-
---
 -- Structure for view `urlsbyid`
---
-DROP TABLE IF EXISTS `urlsbyid`;
-
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `urlsbyid` AS select `urls`.`id` AS `id`,`urls`.`alias` AS `alias`,`urls`.`url` AS `url`,`urls`.`ip` AS `ip`,`urls`.`time` AS `time`,`urls`.`status` AS `status`,count(`visits`.`id`) AS `visitors` from (`urls` join `visits` on((`visits`.`id` = `urls`.`id`))) group by `visits`.`id` order by count(`visits`.`id`) desc;
 
--- --------------------------------------------------------
-
---
 -- Structure for view `urlsbyvisits`
---
-DROP TABLE IF EXISTS `urlsbyvisits`;
-
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `urlsbyvisits` AS select `urls`.`id` AS `id`,`urls`.`alias` AS `alias`,`urls`.`url` AS `url`,`urls`.`ip` AS `ip`,`urls`.`time` AS `time`,`urls`.`status` AS `status`,count(`visits`.`id`) AS `visitors` from (`urls` join `visits` on((`visits`.`id` = `urls`.`id`))) group by `visits`.`id` order by count(`visits`.`id`) desc;
