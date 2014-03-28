d222c83c7f585ae662b4a292fdf5c9bd66a61e4e
Eric OC <eric@ericoc.com>
2014-03-27 21:18:55 -0400

    changed time to INT instead of varchar for now and thinking about timestamp column for the future

--- gawsh.sql
+++ gawsh.sql
@@ -1,19 +1,19 @@
 -- Database: `gawsh`
 -- --------------------------------------------------------
 
--- Table structure for table `urls`
+-- `urls` table
 CREATE TABLE IF NOT EXISTS `urls` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `alias` varchar(50) NOT NULL,
   `url` text NOT NULL,
   `ip` varchar(128) NOT NULL,
-  `time` varchar(15) NOT NULL DEFAULT '',
+  `time` int(10) unsigned NOT NULL,
   `status` varchar(2) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `id` (`id`),
-  KEY `alias` (`alias`),
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+  UNIQUE KEY `alias` (`alias`)
+);
 
+-- few default `urls`
 INSERT INTO `urls` (`id`, `alias`, `url`, `ip`, `time`, `status`) VALUES
 (1, 'admin', '', '127.0.0.1', '1323141765', '-1'),
 (2, '401', '', '127.0.0.1', '1323141766', '-1'),
@@ -23,26 +23,27 @@ INSERT INTO `urls` (`id`, `alias`, `url`, `ip`, `time`, `status`) VALUES
 (6, '503', '', '127.0.0.1', '1323141770', '-1');
 
 
--- Table structure for table `visits`
+-- `visits` table
 CREATE TABLE IF NOT EXISTS `visits` (
   `id` int(10) unsigned NOT NULL,
   `ip` varchar(128) NOT NULL,
   `browser` text NOT NULL,
   `referrer` text NOT NULL,
-  `time` varchar(15) NOT NULL,
-  KEY `visits` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `time` int(10) unsigned NOT NULL,
+  KEY `visits` (`id`),
+  CONSTRAINT `urlid_fk` FOREIGN KEY (`id`) REFERENCES `urls` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
 
 -- --------------------------------------------------------
 
--- Structure for view `recentvisits`
+-- `recentvisits` view
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `recentvisits` AS select `urls`.`alias` AS `alias`,from_unixtime(`visits`.`time`) AS `FROM_UNIXTIME(visits.time)`,`visits`.`ip` AS `ip`,`visits`.`referrer` AS `referrer` from (`visits` join `urls` on((`urls`.`id` = `visits`.`id`))) order by `visits`.`time` desc limit 100;
 
--- Structure for view `searchurlsvisits`
+-- `searchurlsvisits` view
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `searchurlsvisits` AS select `urls`.`id` AS `id`,`urls`.`alias` AS `alias`,`urls`.`url` AS `url`,`urls`.`ip` AS `ip`,`urls`.`time` AS `time`,`urls`.`status` AS `status`,count(`visits`.`id`) AS `visits` from (`urls` join `visits` on((`visits`.`id` = `urls`.`id`))) group by `visits`.`id`;
 
--- Structure for view `urlsbyid`
+-- `urlsbyid` view
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `urlsbyid` AS select `urls`.`id` AS `id`,`urls`.`alias` AS `alias`,`urls`.`url` AS `url`,`urls`.`ip` AS `ip`,`urls`.`time` AS `time`,`urls`.`status` AS `status`,count(`visits`.`id`) AS `visitors` from (`urls` join `visits` on((`visits`.`id` = `urls`.`id`))) group by `visits`.`id` order by count(`visits`.`id`) desc;
 
--- Structure for view `urlsbyvisits`
+-- `urlsbyvisits` view
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `urlsbyvisits` AS select `urls`.`id` AS `id`,`urls`.`alias` AS `alias`,`urls`.`url` AS `url`,`urls`.`ip` AS `ip`,`urls`.`time` AS `time`,`urls`.`status` AS `status`,count(`visits`.`id`) AS `visitors` from (`urls` join `visits` on((`visits`.`id` = `urls`.`id`))) group by `visits`.`id` order by count(`visits`.`id`) desc;
